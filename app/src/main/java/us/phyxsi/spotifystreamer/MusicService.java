8677d1ebec7efd520760474381caf8b1c06f67f8
Andy <thingswithhandles@gmail.com>
2015-07-28 00:10:03 -0500

    Started working on Music Service

--- app/src/main/java/us/phyxsi/spotifystreamer/MusicService.java
+++ app/src/main/java/us/phyxsi/spotifystreamer/MusicService.java
@@ -1,7 +1,15 @@
 package us.phyxsi.spotifystreamer;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.service.media.MediaBrowserService;
+import android.support.annotation.Nullable;
+
 /**
  * Created by arose on 7/20/2015.
  */
-public class MusicService {
+public class MusicService extends MediaBrowserService implements Playback.Callback {
+
+
 }
